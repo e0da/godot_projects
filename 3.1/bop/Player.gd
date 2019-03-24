7d49a8cfefa3db0eac0ae4dc30baab79000c4d05
Justin Force <justin.force@gmail.com>
2019-03-24 00:37:26 -0700

    Support joystick axis for left+right

--- 3.1/bop/Player.gd
+++ 3.1/bop/Player.gd
@@ -3,19 +3,21 @@ extends KinematicBody2D
 export(int) var acceleration : int = 20
 export(float) var friction : float = acceleration*2
 export(int) var gravity : int = 50
-export(int) var jump_force : int = 700
+export(int) var jump_force : int = 500
 export(int) var max_speed : int = 200
 export(Vector2) var floor_vector : Vector2 = Vector2(0, -1)
 export(Vector2) var velocity : Vector2 = Vector2(0,0)
+export(float) var dead_zone : float = 0.2
 
 func _physics_process(_delta):
 	velocity.y += gravity
 	velocity = move_and_slide(velocity, floor_vector)
 	
-	if Input.is_action_pressed("ui_left"):
+	var x_axis_value = Input.get_joy_axis(0, JOY_AXIS_0)
+	if Input.is_action_pressed("ui_left") or x_axis_value <= -dead_zone:
 		velocity.x -= acceleration
 		$Sprite.flip_h = true
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right") or x_axis_value >= dead_zone:
 		velocity.x += acceleration
 		$Sprite.flip_h = false
 	else:
