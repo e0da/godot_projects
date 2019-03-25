27c5c847eab9601a6bc3f71ab1ee64d0d1b51407
Justin Force <justin.force@gmail.com>
2019-03-24 22:23:53 -0700

    Set up actions for input; add fullscreen toggle

--- 3.1/bop/Player.gd
+++ 3.1/bop/Player.gd
@@ -7,17 +7,15 @@ export(int) var jump_force : int = 500
 export(int) var max_speed : int = 200
 export(Vector2) var floor_vector : Vector2 = Vector2(0, -1)
 export(Vector2) var velocity : Vector2 = Vector2(0,0)
-export(float) var dead_zone : float = 0.2
 
 func _physics_process(_delta):
 	velocity.y += gravity
 	velocity = move_and_slide(velocity, floor_vector)
 	
-	var x_axis_value = Input.get_joy_axis(0, JOY_AXIS_0)
-	if Input.is_action_pressed("ui_left") or x_axis_value <= -dead_zone:
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= acceleration
 		$Sprite.flip_h = true
-	elif Input.is_action_pressed("ui_right") or x_axis_value >= dead_zone:
+	elif Input.is_action_pressed("move_right"):
 		velocity.x += acceleration
 		$Sprite.flip_h = false
 	else:
@@ -30,5 +28,5 @@ func _physics_process(_delta):
 	velocity.x = clamp(velocity.x, -max_speed, max_speed)
 		
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_force
--- 3.1/bop/World.gd
+++ 3.1/bop/World.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 func _process(_delta):
-	if Input.is_action_pressed("ui_cancel"):
+	if Input.is_action_pressed("quit"):
 		get_tree().quit()
+
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
