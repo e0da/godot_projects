bcf8d7368fc2a8dafebcf346f98b05fc343ccc13
Justin Force <justin.force@gmail.com>
2019-03-24 23:02:15 -0700

    Hide mouse cursor in full screen

--- 3.1/bop/World.gd
+++ 3.1/bop/World.gd
@@ -6,3 +6,8 @@ func _process(_delta):
 
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+		var hidden = Input.MOUSE_MODE_HIDDEN
+		var shown = Input.MOUSE_MODE_VISIBLE
+		var current_mode = Input.get_mouse_mode()
+		var mouse_mode = hidden if current_mode == shown else shown
+		Input.set_mouse_mode(mouse_mode)
